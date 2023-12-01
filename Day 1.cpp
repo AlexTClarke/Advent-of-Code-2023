05f4c795ccaa2f0549a2f153e3cac84e0869adaf
AlexTClarke <119004428+AlexTClarke@users.noreply.github.com>
2023-12-01 12:41:50 +0000

    Part 1 Complete

--- Day 1.cpp
+++ Day 1.cpp
@@ -3,7 +3,7 @@
 #include <string>
 int main()
 {
-	std::cout << "Hello, world!";
+	std::cout << "Hello, world!\n";
 	std::ifstream inputFile("input.txt");
 
 	if (!inputFile.is_open()) {
@@ -12,10 +12,30 @@ int main()
 	}
 
 	std::string line;
+	int sum = 0;
 	while (std::getline(inputFile, line)) {
-		std::cout << line << '\n';
+		int firstInt = -1;
+		int lastInt = -1;
+		std::cout << line << "\n";
+
+		for (char c : line) {
+			if (std::isdigit(c)) {
+				int letter = (int)c - 48;
+				if (firstInt < 0) {
+					firstInt = letter;
+					lastInt = letter;
+				}
+				else {
+					lastInt = (int)c - 48;
+				}
+			}
+		}
+		std::cout << firstInt << " " << lastInt << "\n\n";
+		sum = sum + 10 * firstInt + lastInt;
+		
 	}
 	inputFile.close();
+	std::cout << sum;
 
 	return 0;
 }
